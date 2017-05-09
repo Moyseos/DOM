c62087753b6ce90d4e0bb313cc02deab624fe1d8
Nikolas Moyseos <nikolasmoyseos@gmail.com>
2017-05-08 22:57:41 -0400

    DOM Chalenge

--- hover.js
+++ hover.js
@@ -1,16 +1,14 @@
 var heading = document.getElementById("heading");
 
 
-<<<<<<< HEAD
 function mouseEnter() {
 	alert("Hey I Told You Not To Hover Over Me!");
 }
 
 heading.addEventListener("mouseenter", mouseEnter);
-=======
+
 function mouseEnter(mouseenter) {
-	mouseenter = alert("Hey I Said Dont Hover over!");
+	alert("Hey I Said Dont Hover Over Me!");
 }
 
 heading.addEventListener("mouseenter", alert);
->>>>>>> 186a94303ddccc19200dcd05355d09a30b760e1b
